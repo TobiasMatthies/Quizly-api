01dbfcd4f3723173226d67237cc28a78e21f866b
TobiasMatthies <tbsmatthies@gmail.com>
2025-09-08 11:38:47 -0300

    finished authentication

--- authentication/api/urls.py
+++ authentication/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import CookieTokenObtainPairView, RegisterAPIView
+from .views import (CookieTokenObtainPairView, LogoutAPIView, RegisterAPIView,
+                    TokenRefreshAPIView)
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
-    path('login/', CookieTokenObtainPairView.as_view(), name='login')
+    path('login/', CookieTokenObtainPairView.as_view(), name='login'),
+    path('logout/', LogoutAPIView.as_view(), name='logout'),
+    path('token/refresh/', TokenRefreshAPIView.as_view(), name='token-refresh')
 ]
--- authentication/api/views.py
+++ authentication/api/views.py
@@ -2,7 +2,10 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from .serializers import (CustomTokenObtainPairSerializer,
                           RegistrationSerializer)
@@ -50,5 +53,49 @@ class CookieTokenObtainPairView(TokenObtainPairView):
             samesite="Lax"
         )
 
-        response.data = {"detail": "Login succesfull", "user": serializer.validated_data["user"]}
+        response.data = {"detail": "Login succesful", "user": serializer.validated_data["user"]}
         return response
+
+
+class LogoutAPIView(APIView):
+    """View for user logout."""
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.COOKIES.get("refresh_token")
+        if refresh_token:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            response = Response({"detail": "Log-Out successful! All Tokens will be deleted. Refresh token is now invalid."}, status=200)
+            response.delete_cookie("access_token")
+            response.delete_cookie("refresh_token")
+            return response
+
+        return Response({"detail": "No refresh token provided."}, status=400)
+
+
+class TokenRefreshAPIView(TokenRefreshView):
+    """View to refresh JWT tokens."""
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.COOKIES.get("refresh_token")
+
+        if refresh_token is None:
+            return Response({"error": "Refresh token not found in cookies"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        serializer = self.get_serializer(data={"refresh": refresh_token})
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+
+        access_token = serializer.validated_data["access"]
+        response = Response()
+
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            httponly=True,
+            secure=True,
+            samesite="Lax"
+        )
+
+        return Response({"detail": "Token refreshed", "acces": access_token}, status=status.HTTP_200_OK)
